765c7b1a8bf0ea54d2d934e9ef996962ad95557c
Vincent Titterton <vince8nt@gmail.com>
2025-07-16 00:57:14 -0700

    implement graph interface

--- src/graph.h
+++ src/graph.h
@@ -10,12 +10,47 @@
 template<typename Adjacency_t, typename VertexStats_t>
 class Graph {
 public:
+    struct Edges {
+    private:
+        const Adjacency_t* begin_;
+        const Adjacency_t* end_;
+    public:
+        Edges(Adjacency_t* edges, edge_ID_t edges_index, vertex_ID_t size) :
+            begin_(edges + edges_index), end_(edges + edges_index + size) {}
+        
+        // getters
+        vertex_ID_t size() const { return size_; }
+        Adjacency_t operator[](vertex_ID_t i) const { return begin_[i]; }
+
+        // Iterator support for edges
+        const Adjacency_t* begin() const { return vertices_; }
+        const Adjacency_t* end() const { return vertices_ + num_vertices_; }
+
+    };
+
     struct Vertex : public VertexStats_t {
+    private:
         const edge_ID_t csr_index_;
         const vertex_ID_t degree_;
+    public:
+        // constructors
+        typename std::enable_if<std::is_same<VertexStats_t, VertexStats>::value, void>::type
+        Vertex(edge_ID_t csr_index, vertex_ID_t degree) :
+            csr_index_(csr_index), degree_(degree) {}
+        typename std::enable_if<!std::is_same<VertexStats_t, VertexStats>::value, void>::type
         Vertex(const VertexStats_t& vertex_stats, edge_ID_t csr_index, vertex_ID_t degree) :
             VertexStats_t(vertex_stats), csr_index_(csr_index), degree_(degree) {}
+
+        // getters
+        vertex_ID_t degree() const { return degree_; }
+        Edges edges() const { return Edges(csr_index_, degree_); }
+
+        bool neighbors(vertex_ID_t target_id) const {
+            return std::binary_search(edges().begin(), edges().end(), target_id);
+        }
     };
+    
+
     Graph(GraphType graph_type,
             edge_ID_t num_edges, Adjacency_t* edges,    
             vertex_ID_t num_vertices, Vertex* vertices
@@ -32,6 +67,18 @@ public:
     Graph& operator=(const Graph&) = delete;
     Graph(Graph&&) = delete;
     Graph& operator=(Graph&&) = delete;
+
+
+    // getters
+    GraphType graph_type() const { return graph_type_; }
+    edge_ID_t num_edges() const { return num_edges_; }
+    vertex_ID_t num_vertices() const { return num_vertices_; }
+    Vertex& operator[](vertex_ID_t i) const { return vertices_[i]; }
+
+    // Iterator support for vertices
+    const Vertex* begin() const { return vertices_; }
+    const Vertex* end() const { return vertices_ + num_vertices_; }
+
 private:
     const GraphType graph_type_;
     const edge_ID_t num_edges_;
@@ -39,7 +86,7 @@ private:
     const vertex_ID_t num_vertices_;
     const Vertex* vertices_;
     
-}
+};
 
 
 
