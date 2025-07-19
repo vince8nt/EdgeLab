174cbf6828d1d4790a62afb0354eb6ddef25ccc3
Vincent Titterton <vince8nt@gmail.com>
2025-07-19 16:57:24 -0700

    setup exit code infrastructure

--- test/verify_undirected.cpp
+++ test/verify_undirected.cpp
@@ -3,41 +3,58 @@
 #include "../src/debug.h"
 #include "../src/cli.h"
 
+// program body for generalized VertexType, EdgeType, and GraphType
 template<typename Vertex_t, typename Edge_t, GraphType Graph_t>
-class Tester {
-public:
-    void run_test(GenType gen_type, int scale, int degree) {
-        // generate edge list
-        Generator<Vertex_t, Edge_t, Graph_t> generator(gen_type, scale, degree);
-        VectorGraph<Vertex_t, Edge_t> vg = generator.Generate();
-        D_.print(vg);
+int verify_undirected(GenType gen_type, int scale, int degree) {
+    if constexpr (Graph_t == GraphType::DIRECTED)
+        std::cout << "warning: graph type mismatch" << std::endl;
+    
+    // Debug<Vertex_t, Edge_t, Graph_t> D_; // Debug for printing graph
 
-        Builder<Vertex_t, Edge_t, Graph_t> builder;
-        Graph<Vertex_t, Edge_t, Graph_t> graph = builder.BuildGraph(vg);
-        D_.print(graph);
-        D_.print_it(graph);
+    // generate edge list
+    Generator<Vertex_t, Edge_t, Graph_t> generator(gen_type, scale, degree);
+    VectorGraph<Vertex_t, Edge_t> vg = generator.Generate();
+    // D_.print(vg);
+
+    // generaate CLI Graph
+    Builder<Vertex_t, Edge_t, Graph_t> builder;
+    Graph<Vertex_t, Edge_t, Graph_t> graph = builder.BuildGraph(vg);
+    // D_.print(graph);
+    // D_.print_it(graph);
+
+    vertex_ID_t v_id = 0;
+    for (auto &v : graph) { // TODO: find a way to get vertex ID from iterator
+        std::cout << v_id << ": ";
+        for (auto e : v.edges()) {
+            if constexpr (WeightedEdgeType<Edge_t>) {
+                // verify both directions have the same weight
+            }
+        }
+        v_id++;
     }
 
-private:
-    Debug<Vertex_t, Edge_t, Graph_t> D_;
-};
+    return 0;
+}
 
-// Functor for running the tester
-struct TesterRunner {
+
+// Functor for dispatching templated function via CLI options
+struct Dispatcher {
     const CLIOptions& opts;
+    int &exit_code;
     template<typename Vertex_t, typename Edge_t, GraphType Graph_t>
     void operator()() const {
-        std::cout << "[EdgeLab CLI] Running Tester with Vertex: " << typeid(Vertex_t).name()
-                  << ", Edge: " << typeid(Edge_t).name()
-                  << ", GraphType: " << (Graph_t == GraphType::UNDIRECTED ? "UNDIRECTED" : "DIRECTED") << std::endl;
-        Tester<Vertex_t, Edge_t, Graph_t> tester;
-        tester.run_test(opts.gen_type, opts.scale, opts.degree);
-        std::cout << "[EdgeLab CLI] Tester finished.\n";
+        exit_code = verify_undirected<Vertex_t, Edge_t, Graph_t>(opts.gen_type, opts.scale, opts.degree);
     }
 };
 
 int main(int argc, char** argv) {
+    const std::string name = "verify_undirected";
     CLIOptions opts = parse_cli(argc, argv);
-    dispatch_types(opts, TesterRunner{opts});
-    return 0;
+    int exit_code = 0;
+    dispatch_types(opts, Dispatcher{opts, exit_code});
+    if (exit_code)
+        std::cerr << name << " failed with exit code: " << exit_code << std::endl;
+    else
+        std::cout << name << " succeeded with exit code: " << exit_code << std::endl;
+    return exit_code;
 }
