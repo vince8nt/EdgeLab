47a5164b89d06e2401dc1703d707f02ba9561be7
Vincent Titterton <vince8nt@gmail.com>
2025-07-26 07:45:46 -0700

    cleanup OpenCL tc + remove warnings

--- examples_opencl/triangle_counting_opencl.cpp
+++ examples_opencl/triangle_counting_opencl.cpp
@@ -63,18 +63,7 @@ long long triangle_counting_opencl(Graph<Vertex_t, Edge_t, Graph_t>& graph) {
     }
     vertex_offsets[num_vertices] = edge_offset;
     
-    // Debug: Print CSR format for verification
-    std::cout << "CSR format - Vertices: " << num_vertices << ", Edges: " << edge_destinations.size() << std::endl;
-    std::cout << "First few vertex offsets: ";
-    for (int i = 0; i < std::min(10, (int)vertex_offsets.size()); i++) {
-        std::cout << vertex_offsets[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "First few edge destinations: ";
-    for (int i = 0; i < std::min(10, (int)edge_destinations.size()); i++) {
-        std::cout << edge_destinations[i] << " ";
-    }
-    std::cout << std::endl;
+
     
     // Create OpenCL buffers
     cl_mem vertices_buffer = opencl.createBuffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
@@ -109,7 +98,7 @@ long long triangle_counting_opencl(Graph<Vertex_t, Edge_t, Graph_t>& graph) {
     // Round up global size to be a multiple of local size
     size_t global_size = ((num_vertices + local_size - 1) / local_size) * local_size;
     
-    std::cout << "Executing triangle counting kernel with " << global_size << " work items" << std::endl;
+
     opencl.executeKernel(triangle_kernel, global_size, local_size);
     
     // Read result
@@ -125,66 +114,22 @@ long long triangle_counting_opencl(Graph<Vertex_t, Edge_t, Graph_t>& graph) {
     
     std::cout << "Total triangles found: " << triangle_count << std::endl;
     
-    // Debug: Let's also verify the CSR format by doing a simple CPU count
-    // This will help us identify if the issue is in the CSR conversion or the kernel
-    cl_uint cpu_triangles = 0;
-    for (vertex_ID_t u = 0; u < num_vertices; u++) {
-        cl_uint u_start = vertex_offsets[u];
-        cl_uint u_end = vertex_offsets[u + 1];
-        for (cl_uint i = u_start; i < u_end; i++) {
-            cl_uint v = edge_destinations[i];
-            if (u >= v) continue;
-            cl_uint v_start = vertex_offsets[v];
-            cl_uint v_end = vertex_offsets[v + 1];
-            for (cl_uint j = v_start; j < v_end; j++) {
-                cl_uint w = edge_destinations[j];
-                if (v >= w) continue;
-                // Binary search for w in u's neighbor list
-                cl_uint left = u_start, right = u_end;
-                while (left < right) {
-                    cl_uint mid = left + (right - left) / 2;
-                    if (edge_destinations[mid] < w) {
-                        left = mid + 1;
-                    } else {
-                        right = mid;
-                    }
-                }
-                if (left < u_end && edge_destinations[left] == w) {
-                    cpu_triangles++;
-                }
-            }
-        }
-    }
-    std::cout << "CPU verification triangles: " << cpu_triangles << std::endl;
-    
     return triangle_count;
 }
 
 // Functor for dispatching templated function via CLI options
 struct OpenCLTriangleDispatcher {
-    int &exit_code;
     template<typename V, typename E, GraphType G>
     void operator()(Graph<V, E, G> &graph) const {
-        try {
-            auto timer = timer_start();
-            long long triangles = triangle_counting_opencl<V, E, G>(graph);
-            auto time = timer_stop(timer);
-            std::cout << "OpenCL Triangle Counting returned: " << triangles << " in " << time << " seconds" << std::endl;
-        }
-        catch (std::exception &e) {
-            std::cerr << "Caught OpenCL Triangle Counting exception: " << e.what() << std::endl;
-            exit_code = 1;
-        }
+        auto timer = timer_start();
+        long long triangles = triangle_counting_opencl<V, E, G>(graph);
+        auto time = timer_stop(timer);
+        std::cout << "OpenCL Triangle Counting returned: " << triangles << " in " << time << " seconds" << std::endl;
     }
 };
 
 int main(int argc, char** argv) {
-    int exit_code = 0;
     CLIOptions opts = parse_cli(argc, argv);
-    dispatch_cli(opts, OpenCLTriangleDispatcher{exit_code});
-    if (exit_code)
-        std::cerr << "Failed with exit code: " << exit_code << std::endl;
-    else
-        std::cout << "Succeeded with exit code: " << exit_code << std::endl;
-    return exit_code;
+    dispatch_cli(opts, OpenCLTriangleDispatcher());
+    return 0;
 } 
