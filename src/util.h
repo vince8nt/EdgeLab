8c090cb9f45679343e3e6ce5b1a9f566fef776a4
Vincent Titterton <vince8nt@gmail.com>
2025-07-27 15:16:36 -0700

    use only headers in source

--- src/util.h
+++ src/util.h
@@ -21,15 +21,35 @@ using edge_ID_t = uint64_t;
 using weight_t = float;
 
 // timer for benchmarking
-std::chrono::steady_clock::time_point timer_start();
-double timer_stop(const std::chrono::steady_clock::time_point &start);
+inline std::chrono::steady_clock::time_point timer_start() { 
+    return std::chrono::steady_clock::now(); 
+}
+
+inline double timer_stop(const std::chrono::steady_clock::time_point &start) {
+    auto end = std::chrono::steady_clock::now(); // Capture end time
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0;
+}
 
 // specification of graph types
 enum class GraphType {
     UNDIRECTED,
     DIRECTED
 };
-std::ostream& operator<<(std::ostream& os, GraphType Graph_t);
+
+inline std::ostream& operator<<(std::ostream& os, GraphType Graph_t) {
+    switch (Graph_t) {
+        case GraphType::UNDIRECTED:
+            os << "Undirected";
+            break;
+        case GraphType::DIRECTED:
+            os << "Directed";
+            break;
+        default:
+            os << "Unknown Graph Type";
+            break;
+    }
+    return os;
+}
 
 // specification of generation types
 enum class GenType {
@@ -37,7 +57,24 @@ enum class GenType {
     WATTS_STROGATZ,  // Watts-Strogatz
     BARABASI_ALBERT, // Barabasi-Albert
 };
-std::ostream& operator<<(std::ostream& os, GenType Gen_t);
+
+inline std::ostream& operator<<(std::ostream& os, GenType Gen_t) {
+    switch (Gen_t) {
+        case GenType::ERDOS_RENYI:
+            os << "Erdos-Renyi";
+            break;
+        case GenType::WATTS_STROGATZ:
+            os << "Watts-Strogatz";
+            break;
+        case GenType::BARABASI_ALBERT:
+            os << "Barabasi-Albert";
+            break;
+        default:
+            os << "Unknown Generation Type";
+            break;
+    }
+    return os;
+}
 
 // Enum definitions for CLI
 enum class CLIVertexType {
